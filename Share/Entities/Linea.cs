a401edbb7baa066e407bc963214f94f27e415d87
Seba <37636377+SEBAMN015@users.noreply.github.com>
2020-10-06 23:56:11 -0300

    Mis clases

--- Share/Entities/Linea.cs
+++ Share/Entities/Linea.cs
@@ -8,5 +8,11 @@ namespace Share.Entities
 {
     class Linea
     {
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+
+        public ICollection<Horario> horarios { get; set; } = new List<Horario>();
+        public ICollection<Tramo> tramos { get; set; } = new List<Tramo>();
     }
 }

b04bb70af9c3b6fe08f92b67651c8d5fedfc88bf
JotaJota96 <jjap96@gmail.com>
2020-10-13 20:41:20 -0300

    preparadas interfaces e implementaciones en DAL

---
+++ DataAccesLayer/Implementations/DAL_Global.cs
@@ -0,0 +1,13 @@
+﻿using DataAccesLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Implementations
+{
+    public class DAL_Global : IDAL_Global
+    {
+    }
+}

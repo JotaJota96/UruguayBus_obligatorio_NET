84162aaaef5e83e08a22258342c4e7acdbbfb42b
JotaJota96 <jjap96@gmail.com>
2020-11-06 03:00:28 -0300

    Paginas de errores hechas pero no habilitadas

---
+++ UruguayBusWeb/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UruguayBusWeb.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+
+        public ViewResult Unauthorized()
+        {
+            Response.StatusCode = 200;
+            return View();
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return View();
+        }
+
+        public ViewResult InternalServerError()
+        {
+            Response.StatusCode = 200;
+            return View();
+        }
+
+    }
+}

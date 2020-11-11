576a4881c71d944d7b0ccad3bda1bd24a23fd67e
JotaJota96 <jjap96@gmail.com>
2020-11-10 22:31:43 -0300

    hecho Registrar usuario (sin validar)

--- UruguayBusWeb/Controllers/GlobalController.cs
+++ UruguayBusWeb/Controllers/GlobalController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Share.Entities;
+using Share.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using UruguayBusWeb.ApiClient;
@@ -25,6 +28,40 @@ namespace UruguayBusWeb.Controllers
         {
             return View();
         }
+
+        // POST: Admin/RegistrarUsuario
+        [HttpPost]
+        public async Task<ActionResult> RegistrarUsuario(FormCollection collection)
+        {
+            // recibe los datos del elemento a registrar y redirige al listado
+            try
+            {
+                Usuario u = new Usuario()
+                {
+                    persona = new Persona()
+                    {
+                        nombre = collection["nombre"],
+                        apellido = collection["apellido"],
+                        correo = collection["correo"],
+                        contrasenia = collection["contrasenia"],
+                        tipo_documento = (TipoDocumento) int.Parse(collection["tipo_documento"]),
+                        documento = collection["documento"],
+                    }
+                };
+
+                u = await up.RegistrarUsuario(u);
+
+                // Llama a la funcion de este controlador (no es una ruta)
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            catch
+            {
+                // redirigir segun ele rror 
+                // Llama a la funcion de este controlador (no es una ruta)
+                return RedirectToAction("InternalServerError", "Error", new { area = "" });
+            }
+        }
+
 
         // **** **** Fin de seccion de Juan **** ****
         // **** **** Inicio de seccion de Sebastian **** ****

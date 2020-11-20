9990a8b393b51011d31e019b962eaac2f1223364
Lucas <LuC31G@gmail.com>
2020-11-19 23:32:05 -0300

    Echo ProximoVehiulo

---
+++ UruguayBusWeb/Models/ProximoVehiculoModel.cs
@@ -0,0 +1,19 @@
+﻿using Share.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace UruguayBusWeb.Models
+{
+    public class ProximoVehiculoModel
+    {
+        public Vehiculo Vehiculo { get; set; }
+        public decimal? latitud { get; set; }
+        public decimal? longitud { get; set; }
+        
+        [DisplayName("Pasaje reservado")]
+        public bool pasaje_reservado { get; set; }
+    }
+}

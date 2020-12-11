afb32f1a5f023c91ab54e2d0ef51f291b3d66a8b
Lucas <LuC31G@gmail.com>
2020-12-11 18:49:47 -0300

    Solucion modificar parada

--- DataAccesLayer/Implementations/DAL_Admin.cs
+++ DataAccesLayer/Implementations/DAL_Admin.cs
@@ -186,8 +186,8 @@ namespace DataAccesLayer.Implementations
 
                     parada par = db.parada.Find(p.id);
                     par.nombre = p.nombre;
-                    par.latitud = p.latitud;
-                    par.longitud = p.longitud;
+                    //par.latitud = p.latitud;
+                    //par.longitud = p.longitud;
                     db.SaveChanges();
                     return ParadaConverter.convert(par);
                 }

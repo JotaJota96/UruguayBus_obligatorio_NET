59bd608265a5143265e6c904e9067f5a86e8d778
JotaJota96 <jjap96@gmail.com>
2020-12-11 21:13:26 -0300

    mejora en ver mas de linea

--- Share/Entities/Linea.cs
+++ Share/Entities/Linea.cs
@@ -18,7 +18,7 @@ namespace Share.Entities
         public ICollection<Horario> horarios { get; set; } = new List<Horario>();
 
         [Required]
-        [DisplayName("Cantidad de tramos")]
+        [DisplayName("Cantidad de paradas")]
         public ICollection<Tramo> tramos { get; set; } = new List<Tramo>();
     }
 }
